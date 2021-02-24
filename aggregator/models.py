1e267fc51b3e68b193c1a974349ac8fb75ca607c
Yuri Ren <developinghuman1015@gmail.com>
2021-02-24 12:41:41 +1000

    design event model

--- aggregator/models.py
+++ aggregator/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Event(models.Model):
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['session_id', 'timestamp'], name='unique event for session'),
+            models.Index(fields=['session_id'], name='session index'),
+            models.Index(fields=['category'], name='category index'),
+            models.Index(fields=['timestamp'], name='timestamp index'),
+        ]
+
+    session_id = models.CharField(max_length=256)
+    category = models.CharField(max_length=256)
+    name = models.CharField(max_length=256)
+    data = models.JSONField(help_text='JSON data')
+    timestamp = models.DateTimeField(help_text='Event occurred timestamp')
+    created_at = models.DateTimeField(auto_now=True)
